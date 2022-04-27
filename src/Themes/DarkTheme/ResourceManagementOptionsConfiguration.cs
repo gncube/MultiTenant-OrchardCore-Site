1f1a0cde72da134dbfa8b8c493acd861ad529fb2
Gerald Ncube <4821668+gncube@users.noreply.github.com>
2022-04-27 08:00:44 +0100

    commit changes

---
+++ src/Themes/DarkTheme/ResourceManagementOptionsConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using OrchardCore.ResourceManagement;
+
+namespace DarkTheme
+{
+    public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
+    {
+        private static ResourceManifest _manifest;
+
+        public ResourceManagementOptionsConfiguration()
+        {
+            _manifest = new ResourceManifest();
+
+            _manifest
+                .DefineStyle("darktheme-mdbootstrap-oc")
+                .SetUrl("~/DarkTheme/css/mdb.dark.min.css")
+                .SetVersion("1.0.0");
+        }
+        public void Configure(ResourceManagementOptions options)
+        {
+            options.ResourceManifests.Add(_manifest);
+        }
+    }
+}
